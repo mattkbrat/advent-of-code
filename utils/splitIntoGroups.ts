d3e8a90ab73b050200b6926488b7ee1e9488a98f
Matthew Bratrsovsky <realzing@pm.me>
2023-10-11 10:15:24 -0600

    chunk arrays

--- utils/splitIntoGroups.ts
+++ utils/splitIntoGroups.ts
@@ -6,3 +6,23 @@ export const text = (input: string) => {
   const secondHalf = input.slice(inputHalfLength, input.length);
   return [firstHalf, secondHalf]
 }
+
+export const array = <T>(input: T[], size: number) => {
+
+  const result = [];
+
+  let newArray: T[] = [];
+
+  for (const i of input) {
+    if (newArray.length < size) {
+      newArray.push(i);
+      continue
+    }
+
+    result.push(newArray)
+    newArray = [i];
+  }
+
+  result.push(newArray)
+  return result;
+}
--- utils/splitIntoGroups_test.ts
+++ utils/splitIntoGroups_test.ts
@@ -1,24 +1,40 @@
 import { assertEquals } from "https://deno.land/std@0.202.0/assert/assert_equals.ts";
-import { text } from "./splitIntoGroups.ts";
-
+import { array, text } from "./splitIntoGroups.ts";
 
 const sampleTests = [{
   input: "vJrwpWtwJgWrhcsFMMfFFhFp",
-  expected: ["vJrwpWtwJgWr", "hcsFMMfFFhFp"]
+  expected: ["vJrwpWtwJgWr", "hcsFMMfFFhFp"],
 }, {
   input: "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-  expected: ["jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"]
-}]
+  expected: ["jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"],
+}];
 
-Deno.test("split text into groups", async (t) => {
+const sampleArrayTests = [{
+  input: ["1", "2", "3"],
+  expected: [["1", "2"], ["3"]],
+}, {
+  input: ["a", "b", "c", "d"],
+  expected: [["a", "b"], ["c", "d"]],
+}];
 
+Deno.test("split string into groups", async (t) => {
   for (const test of sampleTests) {
     await t.step({
       name: test.input,
       fn: () => {
-        assertEquals(text(test.input), test.expected)
-      }
-    })
+        assertEquals(text(test.input), test.expected);
+      },
+    });
   }
+});
 
+Deno.test("chunk arrays into groups of 2", async (t) => {
+  for (const test of sampleArrayTests) {
+    await t.step({
+      name: test.input[0],
+      fn: () => {
+        assertEquals(array(test.input, 2), test.expected);
+      },
+    });
+  }
 });
