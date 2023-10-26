5d23f97e4f93893088a6a0b12d18b7c5346846b6
matt <bratrsovskym@viaero.net>
2023-10-26 16:50:19 -0600

    replace trimming each line with remove line break with regex

--- utils/getInput.ts
+++ utils/getInput.ts
@@ -5,12 +5,15 @@ const getInput = async (filename: string, options: {
   splitChar?: string | null;
   maxLines?: null | number;
 } = defaultOptions) => {
-  const text = (await Deno.readTextFile(`${options.folder}/${filename}`)).trimEnd();
-  const textSplit = (options.splitChar ? text.split(options.splitChar) : [text]).map((s) => s.trim())
-  const textSub = options.maxLines ? textSplit.slice(0, options.maxLines) : textSplit
+  const text = (await Deno.readTextFile(`${options.folder}/${filename}`))
+    .trimEnd();
+  const textSplit = (options.splitChar ? text.split(options.splitChar) : [text])
+    .map((s) => s.replace(/(\r\n|\n|\r)/gm, ""));
+  const textSub = options.maxLines
+    ? textSplit.slice(0, options.maxLines)
+    : textSplit;
 
-  return textSub
-
-}
+  return textSub;
+};
 
 export default getInput;
